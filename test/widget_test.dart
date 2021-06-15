2a883d4b61e5a99d4c8b2a24e4bca653dba10e40
hrithikm2 <h.mishra2feb01@gmail.com>
2021-06-15 15:51:54 +0530

    login work done by Priyanshu

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:aaple_vaavar/Drawer.dart';
+import 'package:aaple_vaavar/login/login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:aaple_vaavar/SplashScreen.dart';
@@ -12,7 +14,7 @@ import 'package:aaple_vaavar/SplashScreen.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(SplashScreen());
+    await tester.pumpWidget(SplashScreen(goToPage: LoginPage(),duration: 3,));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
